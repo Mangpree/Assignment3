1f44c3f2406ae0c6c954d4463061721b87ac6cae
Mangpree <mangpreet36@gmail.com>
2025-04-18 16:33:41 +1200

    Add files via upload

---
+++ Assignment 3/task.py
@@ -0,0 +1,20 @@
+def staff_info():
+    
+    
+    date = input("Enter date: ")
+    staff_id = input("staff_id: ")
+    staff_name = input("staff_name:")
+    
+
+    counter  = 1
+    requisition_id = 10000 + 1
+    print(f"\nPrinting booking information")
+    
+    print(f"date:{date}")
+    print(f"stsff_id:{staff_id}")
+    print(f"staff_name:{staff_name}")
+    print(f"requisition_id:{requisition_id}")
+
+    
+
+staff_info()
